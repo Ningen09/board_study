c1e90e46b0a25e7f67a1034f30b5dbd0e228cf1a
Lee Sugyeong <dltnrud158@daum.net>
2023-08-18 16:29:16 +0900

    Board_DBConnection_Finish

---
+++ board_study/src/test/java/com/board/study/BoardStudyApplicationTests.java
@@ -0,0 +1,83 @@
+package com.board.study;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.board.study.dto.board.BoardRequestDto;
+import com.board.study.dto.board.BoardResponseDto;
+import com.board.study.service.BoardService;
+
+@SpringBootTest
+class BoardStudyApplicationTests {
+	
+	@Autowired
+	private BoardService boardService;
+	
+	
+	@Test
+	void save() {
+		BoardRequestDto boardSaveDto = new BoardRequestDto();
+		
+		boardSaveDto.setTitle("タイトルです");
+		boardSaveDto.setContent("内容です");
+		boardSaveDto.setRegisterId("作成者");
+		
+		Long result = boardService.save(boardSaveDto);
+		
+		if(result > 0) {
+			System.out.println("# Success save() ~");
+			findAll();
+			findById(result);
+		} else {
+			System.out.println("# Fail Save() ~");
+		}
+	}
+
+
+	void findAll() {
+		List <BoardResponseDto> list = boardService.findAll();
+		
+		if(list != null) {
+			System.out.println("# Success findAll() : " + list.toString());
+		} else {
+			System.out.println("#Fail findAll() ~");
+		}
+		
+	}
+
+
+	void findById(Long id) {
+		BoardResponseDto info = boardService.findById(id);
+		
+		if(info != null) {
+			System.out.println("#Success findById() : " + info.toString());
+			updateBoard(id);
+		} else {
+			System.out.println("#Fail findById() ~");
+		}
+		
+	}
+
+
+	void updateBoard(Long id) {
+		
+		BoardRequestDto boardRequestDto = new BoardRequestDto();
+		
+		boardRequestDto.setId(id);
+		boardRequestDto.setTitle("アップデートタイトル");
+		boardRequestDto.setContent("アップデート内容");
+		boardRequestDto.setRegisterId("作成者");
+		
+		int result = boardService.updateBoard(boardRequestDto);
+		
+		if(result > 0) {
+			System.out.println("# Success updateBoard() ~");
+		} else {
+			System.out.println("# Fail updateBoard() ~");
+		}
+	}
+
+}
